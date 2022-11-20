5133bee555e025405af2648a11e3ae307346bbba
POMXARK <1997pom@gmail.com>
2022-11-20 21:16:47 +1000

    Add project files.

---
+++ WpfSQLiteApp/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace WpfSQLiteApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
---
+++ WpfSQLiteApp/User.cs
@@ -0,0 +1,38 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfSQLiteApp
+{
+    public class User : INotifyPropertyChanged
+    {
+        string? name;
+        int age;
+        public int Id { get; set; }
+        public string? Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
